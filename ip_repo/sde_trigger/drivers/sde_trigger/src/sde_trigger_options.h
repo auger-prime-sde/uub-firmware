8ce5fde8ddb153b7b8f045c1ace739811b89f2b5
David Nitz <dfnitz@mtu.edu>
2019-09-08 19:19:28 -0400

    Modified for new test points in V3

--- ip_repo/sde_trigger/drivers/sde_trigger/src/sde_trigger_options.h
+++ ip_repo/sde_trigger/drivers/sde_trigger/src/sde_trigger_options.h
@@ -1,4 +1,4 @@
 #ifndef _TRIGGER_OPTIONS
   #define _TRIGGER_OPTIONS
-  #define COMPILE_DATE 0x14030919
+  #define COMPILE_DATE 0x12080919
 #endif
--- ip_repo/sde_trigger/hdl/sde_trigger_options.vh
+++ ip_repo/sde_trigger/hdl/sde_trigger_options.vh
@@ -1,4 +1,4 @@
 `ifndef _TRIGGER_OPTIONS
   `define _TRIGGER_OPTIONS
-  `define COMPILE_DATE 'h14030919
+  `define COMPILE_DATE 'h12080919
 `endif
