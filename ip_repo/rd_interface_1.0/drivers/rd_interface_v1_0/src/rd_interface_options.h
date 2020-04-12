2d888680cb95dceb3f45b2410cf47e9c161921ac
David Nitz <dfnitz@mtu.edu>
2020-04-12 16:02:52 -0400

    removed obsolete rd_interface_v1_0

--- ip_repo/rd_interface_1.0/drivers/rd_interface_v1_0/src/rd_interface_options.h
+++
@@ -1,4 +0,0 @@
-#ifndef _RD_IFC_OPTIONS
-  #define _RD_IFC_OPTIONS
-  #define RD_IFC_COMPILE_DATE 0x10050319
-#endif
--- ip_repo/rd_interface_1.0/hdl/rd_interface_options.vh
+++
@@ -1,4 +0,0 @@
-`ifndef _RD_IFC_OPTIONS
-  `define _RD_IFC_OPTIONS
-  `define RD_IFC_COMPILE_DATE 'h10050319
-`endif
